f79fb6715421c849c76337c0f572b82d8a7607ff
FabianSchwander <fabian.schwander@students.fhnw.ch>
2022-02-18 14:58:33 +0100

    correct spacing of tiles

--- src/components/TileContainer.tsx
+++ src/components/TileContainer.tsx
@@ -4,7 +4,7 @@ import {Col} from 'reactstrap';
 
 export const TileContainer: React.FC = props => {
   return (
-    <Col className={'TileContainer'} xs={12} sm={6} md={4}>
+    <Col className={'TileContainer'} sm={12} md={6} lg={4}>
       {props.children}
     </Col>);
 };
