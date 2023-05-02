239cfa63772ee83bb9b4c3ede9a06ed784e96d7e
fschwander <fschwander@gmx.ch>
2023-05-02 22:20:19 +0200

    npm upgrades

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,8 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 import App from './App';
-import * as serviceWorker from './services/serviceWorker';
-// @ts-ignore
-import {render} from 'react-snapshot';
 
-render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import {createRoot} from "react-dom/client";
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(<App/>)
